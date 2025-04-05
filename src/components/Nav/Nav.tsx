32cb6effaa7b9867aa0403cb001c258e75ce9da2
m_fktstv <feoktistovm88@mail.ru>
2025-04-06 01:41:57 +0300

    fix(nav): fix multiple accordion opening

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -13,23 +13,23 @@ import { navItems } from './navItems.js';
 export const Nav = () => (
     <Flex
         direction='column'
-        w={250}
-        p={4}
+        w={256}
+        p='10px 16px 10px 10px'
         boxShadow='0 2px 1px -1px rgba(0, 0, 0, 0.2), 0 1px 1px 0 rgba(0, 0, 0, 0.14), 0 1px 3px 0 rgba(0, 0, 0, 0.12)'
     >
-        {navItems.map((i) => (
-            <Accordion key={i.title} allowToggle>
-                <AccordionItem>
-                    <AccordionButton>
+        <Accordion allowToggle>
+            {navItems.map((i) => (
+                <AccordionItem key={i.title}>
+                    <AccordionButton p='8px'>
                         {i.icon}
-                        <Box as='span' flex='1' textAlign='left' ml='4px'>
+                        <Box as='span' flex='1' textAlign='left' ml='12px'>
                             {i.title}
                         </Box>
                         <AccordionIcon />
                     </AccordionButton>
                     <AccordionPanel pb={4}>Lorem ipsum</AccordionPanel>
                 </AccordionItem>
-            </Accordion>
-        ))}
+            ))}
+        </Accordion>
     </Flex>
 );
